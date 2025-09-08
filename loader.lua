cd43a71c174a3a35f602c2349e730a6da5219ef8
tesexrahy <tesexrahy@gmail.com>
2025-09-08 16:57:28 +0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
 -- ID="a7266030-606f-4882-a9fc-e4dfbbb51ce1";
 -- loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
 -- loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
-loadstring(game:HttpGet("https://pastefy.app/6QoeCEAv/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/xvl0RSTs/raw"))()
