e0b1992483d2c2517e7b75b0d48ff14825abd34b
tesexrahy <tesexrahy@gmail.com>
2025-09-21 18:57:13 +0400

    Update rb_script.lua

--- rb_script.lua
+++ rb_script.lua
@@ -1,6 +1,14 @@
 local function loadScript()
---  loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Drifter0507/GUIS/main/MURDER%20MYSTERY%202", true))();
   loadstring(game:HttpGet("https://raw.githubusercontent.com/tesexrahy/script-hub/refs/heads/main/loader_prev.lua"))()
+end
+
+local function loadingScreen()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/xeroxson/script/refs/heads/main/script"))()
+end
+
+local function loadAll() 
+  loadingScreen()
+  loadScripts()
 end
 
 task.spawn(loadScript)
