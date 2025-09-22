c81bd71ed6980ee64825f49ba5461c66b1840dff
tesexrahy <tesexrahy@gmail.com>
2025-09-23 00:56:07 +0400

    Update gag_dupe.lua

--- gag_dupe.lua
+++ gag_dupe.lua
@@ -1,17 +1,1 @@
-local function loadingScreen()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xeroxson/LoadingScreen/refs/heads/main/LoadingScreen.txt"))()
-end
-
-local function loadScript()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tesexrahy/script-hub/refs/heads/main/loader.lua"))()
-end
-
-local function initAll()
-    loadingScreen()
-    loadScript()
-end 
-
-task.spawn(initAll)
-
--- Я еду в Las-Vegas на Comaro мне похуй че ты базаришь
--- Меня везет тип он вставлен грибами и с голубыми волосами
+loadstring(game:HttpGet('https://raw.githubusercontent.com/tesexrahy/script-hub/refs/heads/main/rb_script.lua'))()
